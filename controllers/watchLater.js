e9fa879eead185d9bc4b202cbffa026e8c34c22e
shubham-369 <shubham.srivastav666@gmail.com>
2023-12-29 16:29:42 +0530

    server side

---
+++ controllers/watchLater.js
@@ -0,0 +1,36 @@
+import watchLater from "../models/watchLater.js";
+
+export const watchLaterController = async (req, res) => {
+  const watchLaterData = req.body;
+
+  const addTowatchLater = new watchLater(watchLaterData);
+
+  try {
+    await addTowatchLater.save();
+    res.status(200).json("added to watchLater");
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+export const getAllwatchLaterController = async (req, res) => {
+  try {
+    const files = await watchLater.find();
+    res.status(200).send(files);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+};
+
+export const deletewatchLaterController = async (req, res) => {
+  const { videoId: videoId, Viewer: Viewer } = req.params;
+  try {
+    await watchLater.findOneAndDelete({
+      videoId: videoId,
+      Viewer: Viewer,
+    });
+    res.status(200).json({ message: "Removed  from your watch Laters" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
