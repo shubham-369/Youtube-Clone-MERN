db07180d3ab0d646188bc2191351bd8e272669bc
shubham-369 <shubham.srivastav666@gmail.com>
2023-12-30 14:26:39 +0530

    reconnecting

---
+++ controllers/likeVideo.js
@@ -0,0 +1,34 @@
+import likedVideo from "../models/likedVideo.js";
+
+export const likeVideoController = async (req, res) => {
+  const likedVideoData = req.body;
+  const addToLikedVideo = new likedVideo(likedVideoData);
+  try {
+    await addToLikedVideo.save();
+    res.status(200).json("added to likedVideo");
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+export const getAlllikeVideoController = async (req, res) => {
+  try {
+    const files = await likedVideo.find();
+    res.status(200).send(files);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+};
+
+export const deleteLikeVideoController = async (req, res) => {
+  const { videoId: videoId, Viewer: Viewer } = req.params;
+  try {
+    await likedVideo.findOneAndDelete({
+      videoId: videoId,
+      Viewer: Viewer,
+    });
+    res.status(200).json({ message: "Removed  from your watch Laters" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
